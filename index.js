d948a0646c81c431ca8a0b4227cbf54135d62922
JaiVon <frederickjaivon@yahoo.com>
2022-06-06 17:49:26 -0400

    commit tree 93b851d4bfb99e84649a85d7cbbad9ee75e40630

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber) {
+    let hq = 42
+    return Math.abs(blockNumber - hq);
+}
+
+function distanceFromHqInFeet(blockNumber) {
+    return distanceFromHqInBlocks(blockNumber) * 264;
+}
+
+function distanceTravelledInFeet(blockNumber, destinationBlock) {
+    return Math.abs((blockNumber - destinationBlock) * 264);   
+}
+
+function calculatesFarePrice(blockNumber, destinationBlock) {
+    let distance = Math.abs((blockNumber - destinationBlock) * 264);
+    if ( distance <= 400){
+        return 0
+    } else if (distance > 2000 && distance < 2500) {
+        return 25
+    } else if (distance >= 2500) {
+        return 'cannot travel that far'
+    } else {
+        return (distance - 400) * 0.02
+    }
+
+}
